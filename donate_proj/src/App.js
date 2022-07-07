0c9386d712da109c3f32e5b7d536ca1eba559cc6
mahmudul hasan <mahmudulhasan@mahmuduls-MacBook-Pro.local>
2022-07-07 12:15:44 +0200

    first commit

---
+++ donate_proj/src/App.js
@@ -0,0 +1,12 @@
+
+import Page_1 from './Page_1';
+
+function App() {
+  return (
+    <div >
+      <Page_1></Page_1>
+    </div>
+  );
+}
+
+export default App;
---
+++ donate_proj/src/Page_1.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Page_1 = () => {
+  return (
+    <>
+    <section className='front_page'>
+      <div className='background_1'> </div>
+      <div className='text_box_1'>
+      <h1 className='main_heading'>Let's take responsibility! </h1>
+        <p className='first_para'> Massive floods, collapsed houses, missing and dead people: Severe storms and heavy rainfalls have left widespread destruction in Bangladesh</p>
+        <button className='donate_now_btn'>Donate Now</button>
+      </div>
+      
+
+
+
+
+
+    </section >
+    
+    </>
+  )
+}
+
+export default Page_1
